d5dce8fd291f87c716cb973b5b0211884ddcec04
Emin Taskent <114487171+Emin-Taskent@users.noreply.github.com>
2022-10-03 13:22:11 +0300

    Create odev4.sql

---
+++ odev4.sql
@@ -0,0 +1,22 @@
+-- 1. query
+
+	-- SELECT DISTINCT replacement_cost FROM film
+
+-- 2. query
+
+	-- SELECT COUNT (DISTINCT replacement_cost) FROM 
+
+-- 3. query
+
+	-- SELECT COUNT(*) FROM film
+	-- WHERE title LIKE 'T%' and rating='G'
+
+-- 4. query
+
+	-- SELECT COUNT(*) FROM country
+	-- WHERE country LIKE '_____'
+
+-- 5. query
+
+	-- SELECT COUNT(*) FROM city
+	-- WHERE city ILIKE '%R'
