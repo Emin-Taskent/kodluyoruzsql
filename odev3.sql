ece8f1020fe0ba164bafbe3433d1fee7e049d342
Emin Taskent <114487171+Emin-Taskent@users.noreply.github.com>
2022-10-03 12:08:50 +0300

    Create odev3.sql

---
+++ odev3.sql
@@ -0,0 +1,19 @@
+-- 1. query
+
+	--SELECT country FROM country
+	--WHERE country like 'A%a';
+	
+-- 2. query
+
+	--SELECT country FROM country
+	--WHERE country like '%_____%n'
+	
+-- 3. query
+
+	-- SELECT title FROM film
+	-- WHERE title ILIKE '%t%t%t%t%'
+	
+-- 4. query
+
+	-- SELECT * FROM film
+	-- WHERE title LIKE 'C%' AND length>90 AND rental_rate=2.99
