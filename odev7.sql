686ba2237a8412e324f6e24d613b36542b3d406e
Emin Taskent <114487171+Emin-Taskent@users.noreply.github.com>
2023-01-02 23:24:58 +0300

    Update odev7.sql

--- odev7.sql
+++ odev7.sql
@@ -1,4 +1,5 @@
 -- 1. query
+-- film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
 
 /*
 SELECT rating, COUNT(*) FROM film
@@ -6,6 +7,8 @@ GROUP BY rating
 */
 
 -- 2. query
+-- film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda 
+-- film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
 
 /*
 SELECT replacement_cost, count(*) FROM film
@@ -15,6 +18,7 @@ ORDER BY replacement_cost
 */
 
 -- 3. query
+-- customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
 
 /*
 SELECT store_id, count(*) FROM customer
@@ -22,6 +26,8 @@ GROUP BY
 */
 
 -- 4. query
+-- city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra 
+-- en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
 
 /*
 SELECT country_id, count(*) FROM city
